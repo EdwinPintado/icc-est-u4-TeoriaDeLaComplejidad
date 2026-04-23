501814d7560856f46928c6d6c11b78ff58c16572
Edwin Pintado <edwinpintado2007@gmail.com>
2026-04-22 23:54:33 -0500

    complejidad cambio 1

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,21 @@ import complejidades.ComplejidadNLogN;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("----- COMPLEJIDAD O(1) -----");
+        System.out.println("Demostración de diferentes complejidades algorítmicas\n");
+
+        System.out.println("COMPLEJIDAD O(1) (CONSTANTE) ");
         new ComplejidadConstante().ejemplo();
 
-        System.out.println("\n----- COMPLEJIDAD O(n) -----");
+        System.out.println("\nCOMPLEJIDAD O(n) (LINEAL)");
         new ComplejidadLineal().ejemplo();
 
-        System.out.println("\n----- COMPLEJIDAD O(n^2) -----");
+        System.out.println("\nCOMPLEJIDAD O(n^2) (CUADRÁTICA)");
         new ComplejidadCuadratica().ejemplo();
 
-        System.out.println("\n----- COMPLEJIDAD O(log n) -----");
+        System.out.println("\nCOMPLEJIDAD O(log n) (LOGARÍTMICA)");
         new ComplejidadLogaritmica().ejemplo();
 
-        System.out.println("\n----- COMPLEJIDAD O(n log n) -----");
+        System.out.println("\nCOMPLEJIDAD O(n log n)");
         new ComplejidadNLogN().ejemplo();
     }
 }
